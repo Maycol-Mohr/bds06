88fa474aba51adbf9e5818ce6853484d2eb25e2e
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-01-05 15:28:33 +0100

    React Hook Form first test

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,14 +1,28 @@
 import ButtonIcon from 'components/ButtonIcon';
 
+import { useForm } from 'react-hook-form';
+
 import './styles.css';
 
+type FormData = {
+  username: string;
+  senha: string;
+};
+
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData) => {
+    console.log(formData);
+  };
+
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
+            {...register('username')}
             type="text"
             className="form-control base-input"
             placeholder="Email"
@@ -17,7 +31,8 @@ const Login = () => {
         </div>
         <div className="mb-2">
           <input
-            type="password"
+            {...register('senha')}
+            type="senha"
             className="form-control base-input "
             placeholder="Senha"
             name="senha"
