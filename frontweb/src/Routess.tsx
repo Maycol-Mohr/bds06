7e459bb8fd7a9ccc8222dcfa2b7356f9770c95a1
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-01-13 12:05:27 +0100

    Redirecting to login when 401 or 403

--- frontweb/src/Routess.tsx
+++ frontweb/src/Routess.tsx
@@ -4,33 +4,27 @@ import Home from 'pages/Home';
 import Movies from 'pages/Movies';
 import MoviesId from 'pages/MoviesId';
 
-import {
-  BrowserRouter,
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-} from 'react-router-dom';
+import { Redirect, Route, Router } from 'react-router-dom';
+import history from 'util/history';
 
 const Routess = () => {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Navbar />
-      <Router>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/movies">
-          <Movies />
-        </Route>
-        <Route path="/movies/:movieId">
-          <MoviesId />
-        </Route>
-        <Redirect from="/admin/auth" to="/admin/auth/login" exact />
-        <Route path="/admin/auth">
-          <Auth />
-        </Route>
-      </Router>
-    </BrowserRouter>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/movies">
+        <Movies />
+      </Route>
+      <Route path="/movies/:movieId">
+        <MoviesId />
+      </Route>
+      <Redirect from="/admin/auth" to="/admin/auth/login" exact />
+      <Route path="/admin/auth">
+        <Auth />
+      </Route>
+    </Router>
   );
 };
 
