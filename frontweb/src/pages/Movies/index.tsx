40940183fd5fed103451adf371767d57e3377089
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-01-14 20:42:41 +0100

    algumas configuracoes Movies - MoviesId

--- frontweb/src/pages/Movies/index.tsx
+++ frontweb/src/pages/Movies/index.tsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 const Movies = () => {
   return (
     <>
-      <div className="my-4">
+      <div className="my-4 configurar-movies">
         <h1>Tela listagem de filmes</h1>
       </div>
-      <div>
+      <div className="configurar-link1">
         <Link to="link">Acessar /movies/1</Link>
       </div>
-      <div>
+      <div className="configurar-link2">
         <Link to="link">Acessar /movies/2</Link>
       </div>
     </>
