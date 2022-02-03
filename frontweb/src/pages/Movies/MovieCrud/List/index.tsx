6922a40e226a895157971149ec05cc45d530fc13
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-02-03 18:39:43 +0100

    exercicio 10 continuacao + React Select layout

--- frontweb/src/pages/Movies/MovieCrud/List/index.tsx
+++ frontweb/src/pages/Movies/MovieCrud/List/index.tsx
@@ -1,12 +1,22 @@
 import { ReactComponent as CardFilm } from 'assets/images/Imagem-Card.svg';
 import { Link } from 'react-router-dom';
+import Select from 'react-select';
 
 import './styles.css';
 
 const List = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+
   return (
-    <>
-      <div className="select-item">Aventura</div>
+    <div className="movie-film-container">
+      <div className="select-item">
+        <Select options={options} classNamePrefix="movie-crud-select" isMulti />
+      </div>
+
       <div className="row">
         <div className="card-film2">
           <div className="col-sm-6">
@@ -48,8 +58,108 @@ const List = () => {
             <h3>O olho do inimigo está se movendo.</h3>
           </div>
         </div>
+        <div className="card-film2">
+          <div className="col-sm-6">
+            <Link to={'/movies/:movieId'}>
+              <CardFilm className="image" />
+            </Link>
+            <h1>O Retorno do Rei</h1>
+            <h2>2013</h2>
+            <h3>O olho do inimigo está se movendo.</h3>
+          </div>
+        </div>
+        <div className="card-film2">
+          <div className="col-sm-6">
+            <Link to={'/movies/:movieId'}>
+              <CardFilm className="image" />
+            </Link>
+            <h1>O Retorno do Rei</h1>
+            <h2>2013</h2>
+            <h3>O olho do inimigo está se movendo.</h3>
+          </div>
+        </div>
+        <div className="card-film2">
+          <div className="col-sm-6">
+            <Link to={'/movies/:movieId'}>
+              <CardFilm className="image" />
+            </Link>
+            <h1>O Retorno do Rei</h1>
+            <h2>2013</h2>
+            <h3>O olho do inimigo está se movendo.</h3>
+          </div>
+        </div>
+        <div className="card-film2">
+          <div className="col-sm-6">
+            <Link to={'/movies/:movieId'}>
+              <CardFilm className="image" />
+            </Link>
+            <h1>O Retorno do Rei</h1>
+            <h2>2013</h2>
+            <h3>O olho do inimigo está se movendo.</h3>
+          </div>
+        </div>
+        <div className="card-film2">
+          <div className="col-sm-6">
+            <Link to={'/movies/:movieId'}>
+              <CardFilm className="image" />
+            </Link>
+            <h1>O Retorno do Rei</h1>
+            <h2>2013</h2>
+            <h3>O olho do inimigo está se movendo.</h3>
+          </div>
+        </div>
+        <div className="card-film2">
+          <div className="col-sm-6">
+            <Link to={'/movies/:movieId'}>
+              <CardFilm className="image" />
+            </Link>
+            <h1>O Retorno do Rei</h1>
+            <h2>2013</h2>
+            <h3>O olho do inimigo está se movendo.</h3>
+          </div>
+        </div>
+        <div className="card-film2">
+          <div className="col-sm-6">
+            <Link to={'/movies/:movieId'}>
+              <CardFilm className="image" />
+            </Link>
+            <h1>O Retorno do Rei</h1>
+            <h2>2013</h2>
+            <h3>O olho do inimigo está se movendo.</h3>
+          </div>
+        </div>
+        <div className="card-film2">
+          <div className="col-sm-6">
+            <Link to={'/movies/:movieId'}>
+              <CardFilm className="image" />
+            </Link>
+            <h1>O Retorno do Rei</h1>
+            <h2>2013</h2>
+            <h3>O olho do inimigo está se movendo.</h3>
+          </div>
+        </div>
+        <div className="card-film2">
+          <div className="col-sm-6">
+            <Link to={'/movies/:movieId'}>
+              <CardFilm className="image" />
+            </Link>
+            <h1>O Retorno do Rei</h1>
+            <h2>2013</h2>
+            <h3>O olho do inimigo está se movendo.</h3>
+          </div>
+        </div>
+        <div className="card-film2">
+          <div className="col-sm-6">
+            <Link to={'/movies/:movieId'}>
+              <CardFilm className="image" />
+            </Link>
+            <h1>O Retorno do Rei</h1>
+            <h2>2013</h2>
+            <h3>O olho do inimigo está se movendo.</h3>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
