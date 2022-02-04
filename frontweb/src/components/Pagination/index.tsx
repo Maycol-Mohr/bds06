58f7a22ca17fc91275022922838d2c66e759c82a
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-02-04 13:23:53 +0100

    aula 10-26 Pagination layout

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -5,11 +5,22 @@ import './styles.css';
 
 const Pagination = () => {
   return (
-    <ReactPaginate
-      pageCount={10}
-      pageRangeDisplayed={3}
-      marginPagesDisplayed={1}
-    />
+    <ul>
+      <ReactPaginate
+        pageCount={10}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={1}
+        containerClassName="pagination-container"
+        pageLinkClassName="pagination-item"
+        breakClassName="pagination-item"
+        previousClassName="arrow-previous"
+        nextClassName="arrow-next"
+        activeLinkClassName="pagination-link-active"
+        disabledClassName="arrow-inactive"
+        previousLabel={<ArrowIcon />}
+        nextLabel={<ArrowIcon />}
+      />
+    </ul>
   );
 };
 
